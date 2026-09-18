a391e414019beba9a99dfe274431e7fd566d4322
Cilean77 <jodileisacileanpocong@gmail.com>
2026-09-17 17:30:38 -0700

    Add files via upload

---
+++ movie.java
@@ -0,0 +1,13 @@
+public class movie{
+
+   String title;
+   String genre;
+   int duration;
+   
+   public void displayInfo(){
+      System.out.println("Movie Title: " + title);
+       System.out.println("Movie Genre: " + genre);
+        System.out.println("Movie Duration: " + duration);
+   }
+
+}
